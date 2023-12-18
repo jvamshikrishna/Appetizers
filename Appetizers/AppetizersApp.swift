11058fb4b7b52b7e11a2269b00b8ee9ffecebfc4
jvamshikrishna <jvamshikrishna1997@gmail.com>
2023-12-18 15:13:04 -0600

    Initial Commit

---
+++ Appetizers/AppetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizersApp.swift
+//  Appetizers
+//
+//  Created by Vamshi krishna Jillapally on 12/18/23.
+//
+
+import SwiftUI
+
+@main
+struct AppetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
